5cd378c52f86063e6563bd09d6c373ba8200ad45
albatron34000 <82473077+albatron34000@users.noreply.github.com>
2023-01-23 13:55:25 +0100

    Update building.sh

--- building.sh
+++ building.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Define Variables
-DEVICE="z3s"
-DT="https://github.com/Nico170420/android_device_samsung_z3s.git"
-OEM="samsung"
+DEVICE="RMX2151"
+DT="https://github.com/albatron34000/shrp_device_RMX2151"
+OEM="realme"
 TW_BRANCH="12.1"
 TARGET=(
 	recoveryimage
